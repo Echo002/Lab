4001bee61a727a735711cf3bc88cb619d0688ff8
Echo <xugaohero@163.com>
2019-10-15 21:46:19 +0800

    update

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import numpy as np
+import sys
+print(sys.version)
+print(sys.version_info)
+# import tensorflow as tf
 a = np.array([[1, 2, 3],[4, 5, 6]])
 
-print(np.prod(a))
 print(np.prod(a, axis=0))
 print(np.prod(a, axis=1))
