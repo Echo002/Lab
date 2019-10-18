582c7580fec46bc2110f433570fccfedb776e3bc
Dylan <xugaohero@163.com>
2019-10-18 21:13:31 +0800

    update

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-import numpy as np
-import sys
-print(sys.version)
-print(sys.version_info)
-# import tensorflow as tf
-a = np.array([[1, 2, 3],[4, 5, 6]])
+import tensorflow as tf
 
-print(np.prod(a, axis=0))
-print(np.prod(a, axis=1))
+with tf.device('/cpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0], shape=[3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0], shape=[3], name='b')
+with tf.device('/gpu:1'):
+    c = a + b
+
+# 注意：allow_soft_placement=True表明：计算设备可自行选择，如果没有这个参数，会报错。
+# 因为不是所有的操作都可以被放在GPU上，如果强行将无法放在GPU上的操作指定到GPU上，将会报错。
+sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True))
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+sess.run(tf.global_variables_initializer())
+print(sess.run(c))
